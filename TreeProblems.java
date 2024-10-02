22ce07c346d8de3e71143326057f785f5a813372
mhernandez1531 <142850461+mhernandez1531@users.noreply.github.com>
2024-10-02 17:15:13 -0500

    Update TreeProblems.java

--- TreeProblems.java
+++ TreeProblems.java
@@ -19,19 +19,24 @@ public class TreeProblems {
    * elements that are in one set but not the other.
    */
   
-  public static Set<Integer> different(Set<Integer> setA, Set<Integer> setB) {
+   public static Set<Integer> different(Set<Integer> setA, Set<Integer> setB) {
 
+    // Create new TreeSet named union that starts with all elements from setA
     Set<Integer> union = new TreeSet<>(setA);
+    // Add all elements from setB to the union set
     union.addAll(setB);
 
+    // Create another TreeSet called intersection that starts with elements from SetA
     Set<Integer> intersection = new TreeSet<>(setA);
+    // Only keep elements that are in both setA and setB
     intersection.retainAll(setB);
 
+    // Remove all elements that are in the intersectionfrom the union set
     union.removeAll(intersection);
 
+    // Return the union set that now only contains elements that are in one set but not both
     return union;
   }
-
 
   /**
    * Method removeEven()
@@ -42,11 +47,17 @@ public class TreeProblems {
 
   public static void removeEven(Map<Integer, String> treeMap) {
 
+    // Create an iterator to go through the entries of the TreeMap
     Iterator<Map.Entry<Integer, String>> iterator = treeMap.entrySet().iterator();
+
+    // Loop through the TreeMap using the iterator
     while (iterator.hasNext()) {
+      // Get next entry in the TreeMap
       Map.Entry<Integer, String> entry = iterator.next();
 
+      // Check if keys even
       if (entry.getKey() % 2 == 0) {
+        // If keys even, remove entry from the TreeMap
         iterator.remove();
       }
     }
@@ -61,8 +72,10 @@ public class TreeProblems {
    * return a boolean value indicating if the two trees are equal or not.
    */
 
-  public boolean treesEqual(Map<Integer, String> tree1,Map<Integer, String> tree2 ) {
-
+  public boolean treesEqual(Map<Integer, String> tree1,Map<Integer, String> tree2) {
+   
+    // Use method equals() to check if tree1 and tree2 are the same
+    // It checks both keys and values to see if they match
     return tree1.equals(tree2);
   }
 }
